d183886814e875dee81248fc37e2bf845c2c5c59
ellismar.junior <ellismar.junior@basis.com.br>
2020-03-04 11:46:33 -0300

    introdução javascript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nome = "Junior";
+// alert("Bem vindo " + nome);
+
+var frase = "Japão é o melhor time do mundo";
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase())
