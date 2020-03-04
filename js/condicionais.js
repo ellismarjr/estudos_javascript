4a80166ac70bcca05f4d22e25986d8e1d4d194e5
ellismar.junior <ellismar.junior@basis.com.br>
2020-03-04 16:26:52 -0300

    estudos condicionais

---
+++ js/condicionais.js
@@ -0,0 +1,22 @@
+// var d = new Date();
+// alert(d.getDay() + "/" + (d.getMonth() + 1))
+
+// var idade = prompt("Qual sua idade?")
+// if (idade >= 18) {
+//     alert("maior de idade")
+// } else {
+//     alert("menor de idade")
+// }
+
+// var count = 0;
+
+// while (count <= 5) {
+//     console.log(count)
+//     count++;
+// }
+
+var lista = ["Alemanha", "Inglaterra", "Escócia"]
+lista.push("Polônia");
+console.log(lista)
+lista.pop("Inglaterra");
+console.log(lista)
