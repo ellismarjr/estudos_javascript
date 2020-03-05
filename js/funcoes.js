3cd1557a913cecc0dee22f5483087c4fbecfa707
ellismar.junior <ellismar.junior@basis.com.br>
2020-03-05 09:20:43 -0300

    estudos sobre funcoes

--- js/funcoes.js
+++ js/funcoes.js
@@ -17,7 +17,37 @@ function validaIdade(idade) {
     return validar;
 }
 
+function botao() {
+    // document.querySelector("h3").innerHTML = "Obrigado por clicar!"
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar </b>"
+    // alert("Obrigado por clicar!")
+}
+
+function redirecionar() {
+    // window.open("https://www.basis.com.br/")
+    window.location.href = "https://www.basis.com.br/"
+}
+
+function mouseover(element) {
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    element.innerHTML = "Obrigado por passar o mouse";
+    // alert("Trocar texto")
+}
+
+function trocar(element) {
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    element.innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    // alert("pagina carregada")
+}
+
+function funcaoChange(element) {
+    console.log(element.value)
+}
+
 // alert(soma(5, 10))
 // alert(setReplace("Vai Japão", "Japão", "Brasil"))
-var idade = prompt("Qual sua idade");
-console.log(validaIdade(idade))
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade))
